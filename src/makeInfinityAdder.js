1135ee7390149f38c10f64f4daccfe4d3933bc18
oolcha <olha.bondarenko.job@gmail.com>
2023-09-21 18:00:10 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -6,7 +6,23 @@
  */
 
 function makeInfinityAdder() {
-  // write code here
+  let count = 0;
+
+  const adder = (number) => {
+    if (number !== undefined) {
+      count += number;
+
+      return adder;
+    }
+
+    const result = count;
+
+    count = 0;
+
+    return result;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
